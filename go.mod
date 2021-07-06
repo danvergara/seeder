5674e40af4e35b5b81c28926b6a580dd8b6df3db
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-07-06 13:15:54 -0500

    test(seeder): add a test function to make sure the data is inserted into the database

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- pkg/seeder/seeder_test.go
+++ pkg/seeder/seeder_test.go
@@ -1,10 +1,37 @@
 package seeder_test
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
 
+	"github.com/danvergara/seeder/db/seeds"
 	"github.com/danvergara/seeder/pkg/seeder"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+
+	// postgres driver for testing.
+	_ "github.com/lib/pq"
 )
+
+var (
+	user     string
+	password string
+	host     string
+	port     string
+	dbname   string
+)
+
+func TestMain(m *testing.M) {
+	user = os.Getenv("DB_USER")
+	password = os.Getenv("DB_PASSWORD")
+	host = os.Getenv("DB_HOST")
+	port = os.Getenv("DB_PORT")
+	dbname = os.Getenv("DB_NAME")
+
+	os.Exit(m.Run())
+}
 
 type Foo struct{}
 
@@ -35,4 +62,57 @@ func TestExecuteNoStruct(t *testing.T) {
 			t.Errorf("should not recieve nil as error %v", err)
 		}
 	}
+}
+
+func TestExecuteRealDB(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping short mode")
+	}
+
+	url := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		user,
+		password,
+		host,
+		port,
+		dbname,
+	)
+
+	var count int
+
+	db, err := sqlx.Open("postgres", url)
+	if err != nil {
+		log.Fatalf("error opening a connection with the database: %s\n", err)
+	}
+
+	defer db.Close()
+
+	db.MustExec("TRUNCATE users, roles, products CASCADE;")
+
+	s := seeds.NewSeed(db)
+
+	if err := seeder.Execute(s); err != nil {
+		t.Errorf("error seeding the db at running tests: %s\n", err)
+	}
+
+	err = db.Get(&count, `SELECT COUNT(*) FROM roles`)
+	if err != nil {
+		t.Errorf("error getting the number of roles in db: %s\n", err)
+	}
+
+	assert.Equal(t, 2, count)
+
+	err = db.Get(&count, `SELECT COUNT(*) FROM users`)
+	if err != nil {
+		t.Errorf("error getting the number of users in db: %s\n", err)
+	}
+
+	assert.Equal(t, 100, count)
+
+	err = db.Get(&count, `SELECT COUNT(*) FROM products`)
+	if err != nil {
+		t.Errorf("error getting the number of products in db: %s\n", err)
+	}
+
+	assert.Equal(t, 100, count)
 }
